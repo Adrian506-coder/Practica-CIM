452f1cbd086626c34bcf66bc331b8aa61259b2f3
Adrian506-coder <treviadrian18@gmail.com>
2025-11-05 10:54:31 -0600

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -608,7 +608,7 @@ app.controller("sucursalCtrl", function ($scope, $http, $rootScope, SesionServic
 
     let preferencias = $rootScope.preferencias || {}
     $rootScope.SesionService = SesionService
-    $scope.currentView = 'sucursal'
+    $rootscope.currentView = 'sucursal'
     
     $.get("sucursal/categorias", {
         categoria: "Abarrotes"
@@ -721,6 +721,7 @@ $("#txtBuscarSucursal").on("keypress", function(e) {
 document.addEventListener("DOMContentLoaded", function (event) {
     activeMenuOption(location.hash)
 })
+
 
 
 
